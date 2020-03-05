0a8c49f694e5931e2a732fd3db05b3b7e09061fb
cswenseth <cswenseth@unomaha.edu>
2020-03-04 22:23:06 -0600

    Removed myvenv

--- myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\charles.000\documents\isqa3900\canvas-lite\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
